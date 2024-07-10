5bfa1e4975683ef0d418939a7c26adc063ade1f7
PRATEUSH SHARMA <115470025+prateushsharma@users.noreply.github.com>
2024-07-10 20:21:47 +0530

    Update ContractAddress.js

--- src/components/ContractAddress.js
+++ src/components/ContractAddress.js
@@ -1,3 +1,3 @@
 // ContractAddress.js
 export const CONTRACT_ADDRESS = 'Enter the contract address here'; // Replace with your contract address
-export const privateKey=''
+export const privateKey='NOTHING TO DO RIGHT NOW';
